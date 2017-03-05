1d3f9dc5db714ce8558e8ca3aa7a9fbac643a4b8
Khairul <kyu.helf@gmail.com>
2017-03-05 16:10:16 +0700

    Initial Commit

---
+++ FileViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileViewer
+//
+//  Created by Khairul Fahmi on 3/5/17.
+//  Copyright © 2017 Khairul Fahmi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
